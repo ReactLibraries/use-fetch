585eda21ab81a378fc2f0e9c9dde6e1f99cc042c
空雲 <info@croud.jp>
2021-01-30 17:29:07 +0900

    first commit

---
+++ README.md
@@ -0,0 +1,80 @@
+# @react-libraries/useFetch
+
+## 1. Description
+
+Like useSWR, it caches the retrieved data.
+It is also possible to pass cache data from SSR.
+
+## 2. usage
+
+### 2.1. Basic
+
+```ts
+type fetchOption<T> = {
+  fetch?: fetcherFn<T>; // Default: (key) => fetch(key).then((data) => data.json());
+  initialData?: T;      // first data
+  skip?: boolean;       // If 'true', execution will be skipped
+};
+
+useFetch<T>(key: string, options?: fetchOption<T>): {
+    data?: T;
+    dispatch: (data?: T) => void; //update cache
+    isValidating: boolean;        //fetch in progress
+    error: unknown;               //catch error
+}
+
+mutate<T>(key: string, value?: T | undefined) => void;
+query<T>(key: string) => T;
+
+//SSR
+getDataFromTree(element: ReactElement) => Promise<CachesType>;
+createCache(data: CachesType<unknown> | undefined) => void;
+```
+
+### 2.2. SSR
+
+SSR can be done automatically by passing cache data as follows in Next.js.  
+Use'getDataFromTree' and 'createCache'.
+
+**_app.tsx**
+
+```tsx
+import { AppContext, AppProps } from 'next/app';
+import { createCache, getDataFromTree, CacheType } from '@react-libraries/use-fetch';
+
+const IS_SERVER = !process.browser;
+
+const App = (props: AppProps & { cache: CacheType }) => {
+  const { Component, cache } = props;
+  !IS_SERVER && createCache(cache);
+  return <Component />;
+};
+App.getInitialProps = async ({ Component, router, AppTree }: AppContext) => {
+  const cache = IS_SERVER
+    ? await getDataFromTree(<AppTree Component={Component} pageProps={{}} router={router} />)
+    : {};
+  return { cache };
+};
+export default App;
+```
+
+## 3. example
+
+```tsx
+import { useFetch } from '@react-libraries/use-fetch';
+
+export const Page = () => {
+  const router = useRouter();
+  const { data, error, isValidating, dispatch } = useFetch<DataType>(
+    "URL"
+  );
+  return (
+    <>
+      <button onClick={dispatch()}>Reload</button>
+      {isValidating && <div>Loading</div>}
+      {error && <div>Error</div>}
+      {data && <pre>{JSON.stringify(data,undefined,'  ')}</pre>}
+    </>
+  );
+};
+```
---
+++ src/index.ts
@@ -0,0 +1,151 @@
+import { ReactElement } from 'react';
+import ReactDOMServer from 'react-dom/server';
+import {
+  useGlobalState,
+  getCache,
+  setCache,
+  mutate as globalMutate,
+  query as globalQuery,
+} from '@react-libraries/use-global-state';
+
+export type CacheType<T = unknown> = {
+  data?: T;
+  error?: unknown;
+};
+export type CachesType<T = unknown> = {
+  [key: string]: CacheType<T>;
+};
+
+export type EventType = 'end';
+export const isValidating = () => validating.size !== 0;
+
+const GlobalKey = '@react-libraries/use-fetch';
+const validating = new Set<string>();
+const validatingEvent = () => {
+  if (validating.size === 0) {
+    event.forEach(([name, listener]) => name === 'end' && listener());
+  }
+};
+
+let event: [string, () => void][] = [];
+export const addEvent = (name: EventType, listener: () => void) => {
+  event = [...event, [name, listener]];
+};
+export const removeEvent = (name: EventType, listener: () => void) => {
+  event = event.filter((item) => item[0] !== name || item[1] !== listener);
+};
+export const mutate = <T = unknown>(key: string, value?: T) => {
+  globalMutate<T>([GlobalKey, key], value);
+};
+export const query = <T = unknown>(key: string) => {
+  return globalQuery<T>([GlobalKey, key]);
+};
+
+export const createCache = (data?: CachesType) => {
+  if (data) {
+    setCache(data);
+  }
+};
+export const getDataFromTree = async (element: ReactElement): Promise<CachesType> => {
+  return new Promise<CachesType>((resolve) => {
+    const appStream = ReactDOMServer.renderToStaticNodeStream(element);
+    appStream.read();
+    if (!isValidating()) {
+      resolve(
+        getCache<CacheType>([GlobalKey])
+      );
+    } else {
+      const listener = () => {
+        resolve(
+          getCache<CacheType>([GlobalKey])
+        );
+        removeEvent('end', listener);
+      };
+      addEvent('end', listener);
+    }
+  }).then((cache) =>
+    Object.fromEntries(
+      Object.entries(cache).map(([key, value]) => [
+        key,
+        Object.fromEntries(
+          Object.entries(value).map(([key, value]) => [
+            key,
+            key !== 'error' || typeof value !== 'object'
+              ? value
+              : Object.fromEntries(
+                  Object.entries(Object.getOwnPropertyDescriptors(value)).map(([key, v]) => [
+                    key,
+                    v.value,
+                  ])
+                ),
+          ])
+        ),
+      ])
+    )
+  );
+};
+
+export type fetcherFn<Data> = ((key: string) => Data | Promise<Data>) | null;
+export type fetchOption<T> = {
+  fetch?: fetcherFn<T>;
+  initialData?: T;
+  skip?: boolean;
+};
+let fetchOptions: fetchOption<unknown> = {};
+export const setOptions = (options: fetchOption<unknown>) => {
+  fetchOptions = options;
+};
+export function useFetch<T>(
+  key: string,
+  options?: fetchOption<T>
+): {
+  data?: T;
+  dispatch: (data?: T) => void;
+  isValidating: boolean;
+  error: unknown;
+} {
+  const [data, render] = useGlobalState<CacheType<T> | undefined>(
+    [GlobalKey, key],
+    options?.initialData === undefined
+      ? undefined
+      : {
+          data: options.initialData,
+        }
+  );
+
+  options = { ...fetchOptions, ...options } as fetchOption<T>;
+  if (!options.skip) {
+    if (data?.data === undefined && data?.error === undefined && !validating.has(key)) {
+      validating.add(key);
+      render({});
+      const result = options.fetch ? options.fetch(key) : fetch(key).then((data) => data.json());
+      if (!(result instanceof Promise)) {
+        validating.delete(key);
+        render({ data: result });
+        validatingEvent();
+      } else {
+        result
+          .then((data) => {
+            validating.delete(key);
+            render({ data });
+            validatingEvent();
+          })
+          .catch((error) => {
+            validating.delete(key);
+            render({ error });
+            validatingEvent();
+          });
+      }
+    }
+  }
+  const dispatch = (data?: T) => {
+    if (data === undefined) render({});
+    else render({ data });
+  };
+  return {
+    data: data?.data,
+    error: data?.error,
+    dispatch,
+    isValidating: validating.has(key),
+  };
+}
